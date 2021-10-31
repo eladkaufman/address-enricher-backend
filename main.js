5fb1236b22627a8688a553c4d4913c4913b3820d
kaufma <34608071+kaufma@users.noreply.github.com>
2021-10-31 10:28:36 +0200

    initial commit

--- main.js
+++ main.js
@@ -12,19 +12,19 @@ app.use(express.json());
 app.use('/api/enrich', schoolRouter)
 
 
-app.use((req, res, next) => {
-    const error = new Error("Not found");
-    error.status = 404;
-    next(error);
-});
+// app.use((req, res, next) => {
+//     const error = new Error("Not found");
+//     error.status = 404;
+//     next(error);
+// });
 
-app.use((error, req, res, next) => {
-    res.status = error.status || 500;
-    res.json({
-        error: {
-            message: error.message,
-        },
-    });
-});
+// app.use((error, req, res, next) => {
+//     res.status = error.status || 500;
+//     res.json({
+//         error: {
+//             message: error.message,
+//         },
+//     });
+// });
 
 app.listen(8000);
